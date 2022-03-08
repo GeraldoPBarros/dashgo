4f4fa5af75d9a52025361e357a7ccc3cd08be760
GeraldoBarros <geraldo.barros@neurobots.com.br>
2022-03-08 09:22:27 -0300

    componente ActiveLink para validar e exibir o link selecionado.

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -5,7 +5,7 @@ import {
   LinkProps as ChakraLinkProps,
 } from "@chakra-ui/react";
 import { ElementType } from "react";
-import Link from "next/link";
+import { ActiveLink } from "../ActiveLink";
 
 interface NavLinkProps extends ChakraLinkProps {
   children: string;
@@ -20,13 +20,13 @@ export default function NavLink({
   ...rest
 }: NavLinkProps) {
   return (
-    <Link href={href} passHref>
+    <ActiveLink href={href} passHref>
       <ChakraLink display="flex" alignItems="center" {...rest}>
         <Icon as={icon} fontSize="20" />
         <Text ml="4" fontWeight="medium">
           {children}
         </Text>
       </ChakraLink>
-    </Link>
+    </ActiveLink>
   );
 }
