a5d01ad447a04bda5d8f5dc1f850104ed2f4b537
GeraldoBarros <geraldo.barros@neurobots.com.br>
2022-03-13 13:55:14 -0300

    finalizacao das validacoes dos campos do formulario de cadastro de usuario.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { Flex, Button, Stack } from "@chakra-ui/react";
 
 import { SubmitHandler, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 import { Input } from "../components/Form/Input";
 
@@ -9,8 +11,17 @@ type SignInFormData = {
   password: string;
 };
 
-export default function Home() {
-  const { register, handleSubmit } = useForm();
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required("E-mail obrigatório.").email("E-mail inválido"),
+  password: yup.string().required("Senha obrigatória."),
+});
+
+export default function SigIn() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema),
+  });
+
+  const { errors } = formState;
 
   const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
     await new Promise((resolve) => setTimeout(resolve, 2000));
@@ -33,17 +44,25 @@ export default function Home() {
             name="email"
             type="email"
             label="E-mail"
+            error={errors.email}
             {...register("email")}
           />
           <Input
             name="password"
             type="password"
             label="Senha"
+            error={errors.password}
             {...register("password")}
           />
         </Stack>
 
-        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+          isLoading={formState.isSubmitting}
+        >
           Entrar
         </Button>
       </Flex>
